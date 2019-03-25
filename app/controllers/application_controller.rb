77c4a29c8ab5b3b965c298baca31b23a04e1b111
Davide Santangelo <davide.santangelo@gmail.com>
2019-03-25 17:26:08 +0100

    add jwt token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 require 'pagy/extras/elasticsearch_rails'
 require 'pagy/extras/headers'
+require 'jwt'
 
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 
   Pagy::VARS[:headers] = {page: 'Current-Page', items: 'Per-Page', pages: false, count: 'Total'}
 
--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,4 +3,36 @@ class BaseController < ApplicationController
   include ExceptionHandler
   include Serializer
   include Pagy::Backend
+
+  before_action :require_authentication
+
+  attr_reader :api_key
+
+  def current_token
+    @current_token ||= authenticate_token
+  end
+
+  def require_authentication
+    authenticate_token || render_unauthorized("the access token provided is invalid.")
+  end
+
+  private
+      
+  def render_unauthorized(message)
+    json_error_response(Response::ACCESS_TOKEN_EXCEPTION, message, :unauthorized)
+  end
+
+  def decode_token_payload(token)
+    payload, header = JWT.decode token, ENV['app_secret'], true, { algorithm: 'HS256' }
+
+    payload
+  rescue JWT::DecodeError
+    {}
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      decode_token_payload(token).fetch('api_key', nil) == ENV['api_key']
+    end
+  end
 end
