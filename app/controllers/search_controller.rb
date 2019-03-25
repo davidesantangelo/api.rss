f0bce1743bb9444856d49efbc89aedcaa7dfb0c8
Davide Santangelo <davide.santangelo@gmail.com>
2019-03-25 15:44:43 +0100

    improved search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,16 @@
 class SearchController < BaseController
   def entries
-    records = Entry.pagy_search(params[:q]).records
-    
-    @pagy, @entries = pagy_elasticsearch_rails(records)
+    payload =  
+      {
+        query: {
+          multi_match: {
+            query:    params[:q], 
+            fields: [ "title", "body^2" ] 
+          }
+        }
+      }
+
+    @pagy, @entries = pagy_elasticsearch_rails(Entry.pagy_search(payload))
 
     json_response_with_serializer(@entries, Serializer::ENTRY)
   end
