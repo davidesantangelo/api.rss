978980e4e3a74e9afbec2479f7ecb809554d8064
Davide Santangelo <davide.santangelo@gmail.com>
2019-04-09 17:18:50 +0200

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,14 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/puma.sock"
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
