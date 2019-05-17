7eba2cd3a4009847828e21bc136378e5ae51839c
Davide Santangelo <davide.santangelo@gmail.com>
2019-05-17 17:06:22 +0200

    fix typo

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -56,7 +56,7 @@ class Entry < ApplicationRecord
   def tags
     return [] unless self.annotations.present?
     
-    self.annotations.uniq { |a| a[:id] }.map do |annotation|
+    self.annotations.uniq { |h| h['id'] }.map do |annotation|
       {
         uri: annotation.dig('uri'),
         spot: annotation.dig('spot'),
