357c2974ac4a7e58ba12a0ecec6ac5278593ef4a
Davide Santangelo <davide.santangelo@gmail.com>
2020-04-22 14:06:11 +0200

    Code cleanup and formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  
+
   root 'welcome#index'
 
-  resources :feeds, only: [:index, :show, :create] do
+  resources :feeds, only: %i[index show create] do
     collection do
       get :popular
     end
-    resources :entries, only: [:index, :show] do
+    resources :entries, only: %i[index show] do
       member do
         get :tags
       end
     end
-    resources :logs, only: [:index, :show]
+    resources :logs, only: %i[index show]
     resources :webhooks
   end
 
